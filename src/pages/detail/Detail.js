a9d89934304ecfbe858b30169c7c72dad86dd18e
muzikamin <muzikamin@naver.com>
2024-07-18 22:22:24 +0900

    혼자서 꾸며보는 디테일

--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -1,10 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { movieDetail } from "../../api";
 import { Loading } from "../../components/Loading";
+import styled from "styled-components";
+import { ORIGIN_URL } from "../../constant/imgUrl";
+import { spacing } from "../../GlobalStyled";
+
+// ---------------------------------------------
+
+const Container = styled.div`
+  width: 100%;
+  padding: ${spacing.side};
+  display: flex;
+  flex-direction: row;
+  background-color: #1d1d1d;
+
+  img {
+    width: 50%;
+  }
+`;
+
+const Con = styled.div`
+  width: 50%;
+  padding: ${spacing.side} 50px;
+
+  p {
+    font-size: 20px;
+    line-height: 30px;
+  }
+`;
+
+const Title = styled.div`
+  font-size: 50px;
+  font-weight: 900;
+  margin-bottom: 50px;
+`;
+
+// ---------------------------------------------
 
 export const Detail = () => {
-  const [detailData, setDetailData] = usestate();
-  const [isLoading, setIsLoading] = usestate(true);
+  const [detailData, setDetailData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
@@ -23,19 +58,29 @@ export const Detail = () => {
 
   return (
     <>
-      {" "}
       {isLoading ? (
         <Loading />
       ) : (
         <>
           <Container>
-            <img />
+            <img src={ORIGIN_URL + detailData.poster_path} alt="" />
             <Con>
               <Title>{detailData.title}</Title>
-              <p>{`런타임 : ${detailData.runtime}`}</p>
-              <p>{`런타임 : `}</p>
-              <p>{`런타임 : `}</p>
-              <p>{`런타임 : `}</p>
+              <p>{`런타임 : ${detailData.runtime} 분`}</p>
+              <br />
+              <p>{`개봉일 : ${detailData.release_date}`}</p>
+              <br />
+              <p>
+                장르 <br />
+                {`- ${detailData.genres[0].name}`}
+                <br />
+                {`- ${detailData.genres[1].name}`}
+              </p>
+              <br />
+              <p>
+                줄거리 <br />
+                {detailData.overview}
+              </p>
             </Con>
           </Container>
         </>
